a00047c538bd0dd82a01308a66042eee0eeee29a
badKOT <osipovvilen03@gmail.com>
2024-04-30 07:47:03 +0300

    Init commit

---
+++ src/main/java/self/project/websocket/config/dto/MessageType.java
@@ -0,0 +1,5 @@
+package self.project.websocket.config.dto;
+
+public enum MessageType {
+    CHAT, JOIN, LEAVE
+}
---
+++ src/test/java/self/project/websocket/TestWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package self.project.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
